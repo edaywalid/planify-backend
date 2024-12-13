d09d98840f82936fe3261345e544f2d202c23548
edaywalid <becharwalid9@gmail.com>
2024-12-13 15:09:59 +0100

    added cors layer on gin

--- internal/router/router.go
+++ internal/router/router.go
@@ -7,6 +7,7 @@ import (
 
 func SetupRouter(container *di.Container) *gin.Engine {
 	router := gin.Default()
+	router.Use(container.Middlewares.CorsMiddleWare.CORSMiddleware())
 	NewPingRouter(container).SetupRouter(router)
 	NewAuthRouter(container).SetupRouter(router)
 	NewSwaggerRouter(container).SetupRouter(router)
