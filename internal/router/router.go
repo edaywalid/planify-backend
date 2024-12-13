748e6145232203a5e5b7d2733509ffbede72928b
edaywalid <becharwalid9@gmail.com>
2024-12-13 14:49:54 +0100

    defined core gin router logic

---
+++ internal/router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/edaywalid/devfest-batna24-backend/internal/di"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(container *di.Container) *gin.Engine {
+	router := gin.Default()
+	NewPingRouter(container).SetupRouter(router)
+	NewAuthRouter(container).SetupRouter(router)
+	NewSwaggerRouter(container).SetupRouter(router)
+	return router
+}
