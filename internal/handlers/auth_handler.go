9d100be607b39b090240fb13fa04d22ee8e63e57
edaywalid <becharwalid9@gmail.com>
2024-12-13 15:49:39 +0100

    added logout logic and logs

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -4,15 +4,23 @@ import (
 	"net/http"
 
 	"github.com/edaywalid/devfest-batna24-backend/internal/services"
+	logger "github.com/edaywalid/devfest-batna24-backend/pkg/utils"
 	"github.com/gin-gonic/gin"
 )
 
 type AuthHandler struct {
+	logger      *logger.MyLogger
 	authService *services.AuthService
 }
 
-func NewAuthHandler(authService *services.AuthService) *AuthHandler {
-	return &AuthHandler{authService}
+func NewAuthHandler(
+	logger *logger.MyLogger,
+	authService *services.AuthService,
+) *AuthHandler {
+	return &AuthHandler{
+		logger,
+		authService,
+	}
 }
 
 func (h *AuthHandler) Register(c *gin.Context) {
@@ -23,18 +31,20 @@ func (h *AuthHandler) Register(c *gin.Context) {
 	}
 
 	if err := c.ShouldBindJSON(&input); err != nil {
+		h.logger.LogError().Msgf("failed binding register body : %v", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	err := h.authService.Register(input.FullName, input.Email, input.Password)
 	if err != nil {
+		h.logger.LogError().Msgf("failed registering user : %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user"})
 		return
 	}
 
 	c.JSON(http.StatusCreated, gin.H{
-		"message": "User registered successfully. Please check your email for verification code.",
+		"message": "User registered successfully",
 	})
 }
 
@@ -45,15 +55,21 @@ func (h *AuthHandler) Login(c *gin.Context) {
 	}
 
 	if err := c.ShouldBindJSON(&input); err != nil {
+		h.logger.LogError().Msgf("failed binding register body : %v", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	tokenPair, err := h.authService.Login(input.Email, input.Password)
 	if err != nil {
+		h.logger.LogError().Msgf("failed login user : %v", err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	c.SetCookie("accessToken", tokenPair.AccessToken, 60*60*24*30, "/", "localhost", false, true)
 	c.String(http.StatusOK, "Logged in successfully")
+}
+
+func (h *AuthHandler) Logout(c *gin.Context) {
+	c.SetCookie("accessToken", "", -1, "/", "localhost", false, true)
 }
