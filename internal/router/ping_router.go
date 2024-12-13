164f05c4d5edff6ace30ec08ef5bb490cae46eec
edaywalid <becharwalid9@gmail.com>
2024-12-13 14:49:18 +0100

    added ping router

---
+++ internal/router/ping_router.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	"github.com/edaywalid/devfest-batna24-backend/internal/di"
+	"github.com/gin-gonic/gin"
+)
+
+type PingRouter struct {
+	container *di.Container
+}
+
+func NewPingRouter(container *di.Container) *PingRouter {
+	return &PingRouter{container: container}
+}
+
+func (pr *PingRouter) SetupRouter(r *gin.Engine) {
+	pingGroup := r.Group("/ping")
+	{
+		pingGroup.GET("", pr.Ping)
+	}
+}
+
+func (pr *PingRouter) Ping(c *gin.Context) {
+	pr.container.Handlers.PingHandler.Ping(c)
+}
