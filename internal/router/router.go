9905cfd10417b4f76e769b50c37aec7280d00f88
edaywalid <becharwalid9@gmail.com>
2024-12-13 20:13:57 +0100

    updated cors handling in gin router

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,12 +2,18 @@ package router
 
 import (
 	"github.com/edaywalid/devfest-batna24-backend/internal/di"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(container *di.Container) *gin.Engine {
 	router := gin.Default()
-	router.Use(container.Middlewares.CorsMiddleWare.CORSMiddleware())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://devfest-batna24-frontend.vercel.app"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type"},
+		AllowCredentials: true,
+	}))
 	NewPingRouter(container).SetupRouter(router)
 	NewAuthRouter(container).SetupRouter(router)
 	NewSwaggerRouter(container).SetupRouter(router)
